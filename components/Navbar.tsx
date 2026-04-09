41b5617a529f27049954bfb82773c8f0be1fa62e
Chan <waanchee@gmail.com>
2026-04-09 20:32:39 +0800

    Comment on unused links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar = () => {
 
         <ul>
           <Link href="/">Home</Link>
-          <Link href="/">Events</Link>
-          <Link href="/">Create Event</Link>
+          {/* <Link href="/">Events</Link>
+          <Link href="/">Create Event</Link> */}
         </ul>
       </nav>
     </header>
